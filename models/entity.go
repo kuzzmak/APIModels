cb98f216a4b826d993e66095650b0df60607a1db
kuzzmak <kuzminski.antonio@gmail.com>
2020-09-12 23:03:20 +0200

    initial commit

---
+++ models/entity.go
@@ -0,0 +1,17 @@
+package models
+
+import "sync"
+
+type Entity struct {
+	Id          string  `json:"imdbId"`
+	Name        string  `json:"name"`
+	PosterId    string  `json:"posterId"`
+	PosterLink  string  `json:"posterLink"`
+	Duration    string  `json:"duration"`
+	ReleaseDate string  `json:"releaseDate"`
+	Rating      float32 `json:"rating"`
+	Type        string  `json:"type"`
+	mux         sync.Mutex
+}
+
+
---
+++ models/movie.go
@@ -0,0 +1,5 @@
+package models
+
+type Movie struct {
+	Entity
+}
