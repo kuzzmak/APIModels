7967258de2e13eb3de465f3f2441715fe55daf88
kuzzmak <kuzminski.antonio@gmail.com>
2020-09-14 00:51:33 +0200

    added episode number field

--- models/episode.go
+++ models/episode.go
@@ -4,6 +4,7 @@ type Episode struct {
 	Id          string  `json:"id"`
 	SeriesId    string  `json:"seriesId"`
 	Season      string  `json:"season"`
+	EpNum       string  `json:"epNum"`
 	Name        string  `json:"name"`
 	Rating      float32 `json:"rating"`
 	Description string  `json:"description"`
