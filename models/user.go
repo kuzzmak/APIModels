6d979c41f4f46b77fb7bbfc9c77db42bd1ccab27
kuzzmak <kuzminski.antonio@gmail.com>
2020-09-21 22:39:56 +0200

    added new model

---
+++ models/user.go
@@ -0,0 +1,13 @@
+package models
+
+import "time"
+
+type User struct {
+	Id        int64
+	FirstName string
+	LastName  string
+	UserName  string
+	Email     string
+	Password  string
+	Birthday  time.Time
+}
