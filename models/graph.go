cce63bec0c45eb1d52798622768c72282a129b36
kuzzmak <kuzminski.antonio@gmail.com>
2020-10-04 21:54:05 +0200

    added struct for graph data

--- models/graph.go
+++ models/graph.go
@@ -4,3 +4,16 @@ type MonthlyGraphDataRequest struct {
 	UserId int64
 	Month  int
 }
+
+type Graph struct {
+	Label        string
+	Labels       []string
+	Data         []float32
+	CurrentYear  int
+	CurrentMonth int
+	CurrentDay   int
+	Colors       []string
+	Min          float32
+	Max          float32
+	Months       []int
+}
